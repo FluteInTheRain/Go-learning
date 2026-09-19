a711896efa5e1ec237793d008866ce2af0de8414
FluteInTheRain <khangnl2000@gmail.com>
2026-09-19 16:12:52 +0700

    feat: week01 - lesson02 - exercise02

---
+++ week01/lesson02/exercise02/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var math float64 = 8.5
+	var physics float64 = 7.0
+	var chemistry float64 = 9.5
+	fmt.Printf("Trung binh cong 3 mon hoc Toan, Ly, Hoa: %.2f\n", (math+physics+chemistry)/3)
+}
