7b644603a61ebaddbe11e48ebd765afebfe62175
FluteInTheRain <khangnl2000@gmail.com>
2026-09-19 16:04:33 +0700

    feat: week01 - lesson02 - exercise01

---
+++ week01/lesson02/exercise01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 15
+	var b int = 4
+	fmt.Printf("%d + %d = %d\n", a, b, a+b)
+	fmt.Printf("%d - %d = %d\n", a, b, a-b)
+	fmt.Printf("%d * %d = %d\n", a, b, a*b)
+	fmt.Printf("%d / %d = %d\n", a, b, a/b)
+}
